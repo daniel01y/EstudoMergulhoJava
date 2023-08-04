87d6469238c9383dff92b01d88754016a071b65b
Daniel Yuji <66379966+daniel01y@users.noreply.github.com>
2023-08-04 20:25:42 -0300

    Add files via upload

---
+++ mergulho-java/src/CalculadoraCortisol.java
@@ -0,0 +1,19 @@
+public class CalculadoraCortisol {
+    public static void main(String[] args) {
+        double cortisol = 18.2;
+
+        boolean resultadoNormal = cortisol >= 6.0 && cortisol <= 18.4;
+//        boolean resultadoAnormal = cortisol < 6.0 || cortisol > 18.4;
+//
+//        System.out.println("Cortisol Normal: " + resultadoNormal);
+//        System.out.println("Cortisol anormal: " + resultadoAnormal);
+
+        if (cortisol >= 6.0 && cortisol <= 18.4) {
+            System.out.println("Cortisol normal");
+        } else if (cortisol > 18.4) {
+            System.out.println("Cortisol alto");
+        } else {
+            System.out.println("Cortisol baixo");
+        }
+    }
+}
---
+++ mergulho-java/src/OlaMergulhador.java
@@ -0,0 +1,26 @@
+public class OlaMergulhador {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mergulhador!");
+
+        String nomeCompleto;
+        nomeCompleto = "João Da Silva";
+
+        System.out.println("Nome: " + nomeCompleto);
+
+        int minhaIdade = 41;
+        int suaIdade = 25;
+        int totalIdades = minhaIdade + suaIdade;
+        System.out.println(totalIdades);
+
+        double peso = 84.9;
+        System.out.println(peso);
+
+        float taxa = 1_294.93F;
+
+        boolean compraAprovada = false;
+        System.out.println(compraAprovada);
+
+        boolean maiorDeIdade = minhaIdade != 42;
+        System.out.println(maiorDeIdade);
+    }
+}
